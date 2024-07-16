4f3d1790d88d4f1fe6041c6c2578c7f5fe5f34c7
evertonse <evertonse.junior@gmail.com>
2024-07-16 17:23:05 -0300

    feat(scope_buffer_close): Better corner cases handling

--- lua/scope/core.lua
+++ lua/scope/core.lua
@@ -68,6 +68,27 @@ function M.print_summary()
     end
 end
 
+-- Check if it exists in a tab other than the CURRENT one
+M.exists_in_other_tabs = function(bufnr)
+    M.revalidate()
+    local current_tab = vim.api.nvim_get_current_tabpage()
+    local buffer_exists_in_other_tabs = false
+    for tab, buffers in pairs(M.cache) do
+        if tab ~= current_tab then
+            for _, buffer in ipairs(buffers) do
+                if buffer == bufnr then
+                    buffer_exists_in_other_tabs = true
+                    break
+                end
+            end
+        end
+        if buffer_exists_in_other_tabs then
+            break
+        end
+    end
+    return buffer_exists_in_other_tabs
+end
+
 -- Smart closing of a scoped buffer, this makes sure you only delete a buffer if is not currently open in any other tab.
 -- If it is, then we just unlist the buffer.
 -- Also if it is the only buffer in the current tab, we close the tab
@@ -78,58 +99,91 @@ end
 ---@param opts.ask boolean, default to true to ask before closing the last tab ---@diagnostic disable-line
 ---@return nil
 M.close_buffer = function(opts)
-    opts = opts or {}
-    local current_tab = vim.api.nvim_get_current_tabpage()
-    local current_buf = opts.buf or vim.api.nvim_get_current_buf()
+    opts = vim.tbl_deep_extend("force", {
+        buf = vim.api.nvim_get_current_buf(),
+        force = true,
+        ask = true,
+    }, opts or {})
+
+    local underlying_buffer_delete = function()
+        if not vim.api.nvim_buf_is_valid(opts.buf) then
+            return
+        end
+        local ok, _ = pcall(vim.api.nvim_buf_delete, opts.buf, { force = opts.force })
+        if not ok then
+            local path = vim.api.nvim_buf_get_name(opts.buf)
+            local choice = vim.fn.confirm("Unsaved changed on " .. path .. "Do you want to force it?", "&Yes\n&No")
+            if choice == 1 then
+                vim.api.nvim_buf_delete(opts.buf, { force = true })
+            end
+        end
+    end
+
+    local underlying_buffer_unlist = function()
+        if not vim.api.nvim_buf_is_valid(opts.buf) or not vim.api.nvim_buf_get_option(0, "buflisted") then
+            return
+        end
+
+        local ok, _ = pcall(vim.api.nvim_buf_set_option, opts.buf, "buflisted", false)
+
+        if not ok then
+            error("Scope: couldn't unlist buffer " .. tostring(opts.buf))
+            return
+        end
+
+        if #utils.get_valid_buffers() >= 1 then
+            vim.cmd("bprev")
+        end
+    end
 
     -- Ensure the cache is up-to-date
     M.revalidate()
 
-    local buffers_in_current_tab = M.cache[current_tab]
-
-    -- Check if the buffer exists in other tabs (could be a utils function)
-    local buffer_exists_in_other_tabs = false
-    for tab, buffers in pairs(M.cache) do
-        if tab ~= current_tab then
-            for _, buffer in ipairs(buffers) do
-                if buffer == current_buf then
-                    buffer_exists_in_other_tabs = true
-                    break
-                end
-            end
-        end
-        if buffer_exists_in_other_tabs then
-            break
-        end
-    end
+    -- Why which version is correct to get the buffers in current tab
+    -- local buf_nums = M.cache[current_tab]
 
     -- If the buffer exists in other tabs, hide it in the current tab
-    if buffer_exists_in_other_tabs then
-        if #buffers_in_current_tab > 1 then
-            vim.cmd([[bprev]])
-            vim.api.nvim_buf_set_option(current_buf, "buflisted", false)
-        else
+
+    local buffer_exist_in_other_tab = M.exists_in_other_tabs(opts.buf)
+    local buffers = utils.get_valid_buffers()
+    if buffer_exist_in_other_tab then
+        underlying_buffer_unlist()
+        if #buffers <= 1 then -- implicit that other tabs exist
+            print("last buffer on tab, closing tab")
             vim.cmd("tabclose")
         end
-    else -- buffer does not exist in other tabs
-        local tab_count = #vim.api.nvim_list_tabpages()
-        if #buffers_in_current_tab == 1 then
-            if tab_count > 1 then
-                vim.api.nvim_buf_delete(current_buf, { force = opts.force })
-                vim.cmd("tabclose")
-            else
-                -- Ask for confirmation before quitting if it's the only tab
-                local choice = vim.fn.confirm("You're about to close the last tab. Do you want to quit?", "&Yes\n&No")
-                if choice == 1 then
-                    vim.cmd("qa!")
+    else
+        -- Can be one both before and after deleting because we might have tried to close the last buffer and
+        -- silently couldn't as a result we might be on the [No Name] buffer
+        local tabs = vim.api.nvim_list_tabpages()
+        if #buffers == 1 then
+            if #tabs == 1 then
+                local just_quit = true
+                if opts.ask then
+                    -- Ask for confirmation before quitting if it's the ONLY tab
+                    local choice =
+                        vim.fn.confirm("You're about to close the last tab. Do you want to quit?", "&Yes\n&No")
+                    just_quit = just_quit and (choice == 1)
                 end
+
+                if just_quit then
+                    vim.cmd("qa!")
+                    return
+                end
+            else
+                -- NOTE: We need to check this the case where we haver 1 buffer and more than 1 (>1) tab
+                -- because the underlying_buffer_delete will not automatically change tabs upon deletion
+                vim.cmd("tabclose") -- more than 1 tab so it's safe to close
             end
         else
-            vim.api.nvim_buf_delete(current_buf, { force = opts.force })
+            -- NOTE: Don't need to check anything else on the more than one buffer case,
+            -- because nvim buffer delete will change focus to previous buffer regardless of number of tabs.
+            -- So nothing needs to be done or checked.
+            underlying_buffer_delete()
         end
     end
 
-    -- Update the cache
+    -- If we got here, this buffer is unique to the current tab
     M.revalidate()
 end
 
